adfc20b27b38663418c2bd1357c3584a9a0b7a92
ziaul sarker <ziaulsarker@gmail.com>
2020-04-11 00:30:08 -0400

    implement basic drag and drop

--- src/react/layouts/home.jsx
+++ src/react/layouts/home.jsx
@@ -1,12 +1,19 @@
 import React, { Fragment } from "react";
 import CreateExercise from "../components/createExercise/createExercise.jsx"
+import DragDropBox from "../components/dragDropBox/dragDropBox.jsx";
+import Dragable from "../components/dragable/dragable";
 
 function HomePage (props){
     return(
         <Fragment>
             <CreateExercise></CreateExercise>
+
+            <DragDropBox></DragDropBox>
+            <Dragable></Dragable>
+            <Dragable id="push-up-2" val="pull up"></Dragable>
+            <DragDropBox></DragDropBox>
         </Fragment>
     )
 }
 
-export default HomePage
+export default HomePage;
